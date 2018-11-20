dc6d093c2de3c0811232c4ba193f489ce76b6ce7
Iago Nóvoa González <ianovoagonzalez@gmail.com>
2018-11-20 18:24:10 +0100

    Cosas nuevas

--- app/src/main/java/es/uvigo/ingonzalezesei/blackjackapp/MainGame.java
+++ app/src/main/java/es/uvigo/ingonzalezesei/blackjackapp/MainGame.java
@@ -4,6 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainGame extends AppCompatActivity {
+    private int puntJugador; //puntuacion total de jugador en esta ronda
+    private int puntMaquina;
+    private int dinGanado; //dinero total ganado por el jugador
+    private int dinApostado; //dinero apostado en esta ronda
+
+    public int getDinApostado() {
+        return dinApostado;
+    }
+
+    public int getDinGanado() {
+        return dinGanado;
+    }
+
+    public int getPuntJugador() {
+        return puntJugador;
+    }
+
+    public int getPuntMaquina() {
+        return puntMaquina;
+    }
+
+    public void setDinApostado(int dinApostado) {
+        this.dinApostado = dinApostado;
+    }
+
+    public void setDinGanado(int dinGanado) {
+        this.dinGanado = dinGanado;
+    }
+
+    public void setPuntJugador(int puntJugador) {
+        this.puntJugador = puntJugador;
+    }
+
+    public void setPuntMaquina(int puntMaquina) {
+        this.puntMaquina = puntMaquina;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
