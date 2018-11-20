6f6bd74fcbd1fa6b8dd04d771b970dc5133cdf17
Iago Nóvoa González <ianovoagonzalez@gmail.com>
2018-11-21 00:05:47 +0100

    Añadidas mas variables al main
    Añadidas variables para la baraja y los elementos de la interfaz (botones, textos, etc...), quite los sets y gets pues no hacen falta al final

--- app/src/main/java/es/uvigo/ingonzalezesei/blackjackapp/MainGame.java
+++ app/src/main/java/es/uvigo/ingonzalezesei/blackjackapp/MainGame.java
@@ -2,44 +2,37 @@ package es.uvigo.ingonzalezesei.blackjackapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainGame extends AppCompatActivity {
     private int puntJugador; //puntuacion total de jugador en esta ronda
-    private int puntMaquina;
+    private int puntMaquina; //idem maquina
     private int dinGanado; //dinero total ganado por el jugador
     private int dinApostado; //dinero apostado en esta ronda
+    private Baraja baraja=Baraja.getBaraja(); //baraja utilizada por el juego
+    /*
+    this.baraja.getCarta(); -> recibe objeto Carta de la baraja
+    this.baraja.finalRonda(); -> reinicia la baraja una vez acabada una ronda
+    */
 
-    public int getDinApostado() {
-        return dinApostado;
-    }
+    //variables botones interfaz
+    private final Button btnRecibir=this.findViewById(R.id.recibir);
+    private final Button btnPlantarse=this.findViewById(R.id.plantarse);
+    private final Button btnApostar=this.findViewById(R.id.apostarBoton);
 
-    public int getDinGanado() {
-        return dinGanado;
-    }
+    //variables textos interfaz
+    private final TextView turno=this.findViewById(R.id.turno);
+    private final TextView dinero=this.findViewById(R.id.dinero);
+    private final TextView puntJugador=this.findViewById(R.id.puntPropia);
+    private final TextView puntCasa=this.findViewById(R.id.puntCasa);
+    private final TextView apuesta=this.findViewById(R.id.apostadoTotal);
 
-    public int getPuntJugador() {
-        return puntJugador;
-    }
+    private final ImageView imagenCarta=this.findViewById(R.id.imageView); //variable imagen interfaz
 
-    public int getPuntMaquina() {
-        return puntMaquina;
-    }
-
-    public void setDinApostado(int dinApostado) {
-        this.dinApostado = dinApostado;
-    }
-
-    public void setDinGanado(int dinGanado) {
-        this.dinGanado = dinGanado;
-    }
-
-    public void setPuntJugador(int puntJugador) {
-        this.puntJugador = puntJugador;
-    }
-
-    public void setPuntMaquina(int puntMaquina) {
-        this.puntMaquina = puntMaquina;
-    }
+    private final EditText cantidadApostada=this.findViewById(R.id.cantidad); //variable EditText interfaz
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
