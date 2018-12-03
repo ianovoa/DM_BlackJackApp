628a79836da7e43e509f48dce98d782efa95cf66
Iago Nóvoa González <ianovoagonzalez@gmail.com>
2018-12-03 03:59:32 +0100

    Ya se abre la app
    ALELUYA... pero ahora falla las funciones de espera

--- app/src/main/java/es/uvigo/ingonzalezesei/blackjackapp/MainGame.java
+++ app/src/main/java/es/uvigo/ingonzalezesei/blackjackapp/MainGame.java
@@ -1,6 +1,7 @@
 package es.uvigo.ingonzalezesei.blackjackapp;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,11 +18,11 @@ public class MainGame extends AppCompatActivity {
     private int dinGanado; //dinero total ganado por el jugador
     private int dinApostado; //dinero apostado en esta ronda
 
-    private byte estado;
-    private final byte CARTA1=1; //estado: primera carta jugador
-    private final byte CARTA2=2; //estado: resto cartas jugador
-    private final byte MAS21=3; //estado: el jugador se pasa de 21
-    private final byte CASA=4; //estado: turno maquina
+    private int estado;
+    private final int CARTA1=1; //estado: primera carta jugador
+    private final int CARTA2=2; //estado: resto cartas jugador
+    private final int MAS21=3; //estado: el jugador se pasa de 21
+    private final int CASA=4; //estado: turno maquina
 
     private Baraja baraja=Baraja.getBaraja(); //baraja utilizada por el juego
     /*
@@ -30,16 +31,35 @@ public class MainGame extends AppCompatActivity {
     */
 
     //variables botones interfaz
-    private final Button btnRecibir=this.findViewById(R.id.recibir);
-    private final Button btnPlantarse=this.findViewById(R.id.plantarse);
-    private final Button btnApostar=this.findViewById(R.id.apostarBoton);
+    private Button btnRecibir;
+    private Button btnPlantarse;
+    private Button btnApostar;
 
-    private final ImageView imagenCarta=this.findViewById(R.id.imageView); //variable imagen interfaz
+    private ImageView imagenCarta; //variable imagen interfaz
+
+    //variables textos
+    private TextView turno;
+    private TextView apuesta;
+    private TextView dinero;
+    private TextView textPuntCasa;
+    private TextView textPuntJugador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
+        this.btnRecibir=this.findViewById(R.id.recibir);
+        this.btnPlantarse=this.findViewById(R.id.plantarse);
+        this.btnApostar=this.findViewById(R.id.apostarBoton);
+
+        this.imagenCarta=this.findViewById(R.id.imageView);
+
+        this.turno=this.findViewById(R.id.turno);
+        this.apuesta=this.findViewById(R.id.apostadoTotal);
+        this.dinero=this.findViewById(R.id.dinero);
+        this.textPuntCasa=this.findViewById(R.id.puntCasa);
+        this.textPuntJugador=this.findViewById(R.id.puntPropia);
 
         //variables inicializadas
         this.setPuntJugador(0);
@@ -77,7 +97,7 @@ public class MainGame extends AppCompatActivity {
         this.setPuntMaquina(prefs.getInt("puntMaquina",0));
         this.setDinGanado(prefs.getInt("dinGanado",1000));
         this.setDinApostado(prefs.getInt("dinApostado",0));
-        this.setEstado(Byte.parseByte(Integer.toString(prefs.getInt("estado",this.CARTA1))));
+        this.setEstado(prefs.getInt("estado",this.CARTA1));
     }
 
     private void apostar(){
@@ -93,75 +113,84 @@ public class MainGame extends AppCompatActivity {
     private void recibirCarta(){ //el jugador pide otra carta pero sin apostar
         Carta carta=this.baraja.getCarta();
         this.setPuntJugador(this.puntJugador + carta.getValue());
-        this.imagenCarta.setImageResource(this.getResources().getIdentifier("DM_BlackJackApp:"+carta.getImage(),null,null));
+        this.imagenCarta.setImageResource(this.getResources().getIdentifier(carta.getImage(),"drawable",getPackageName()));
         if(this.puntJugador>21) this.setEstado(this.MAS21);
     }
 
     private void turnoMaquina(){ //la maquina intenta superar la puntuacion del jugador sin pasarse de 21
+        Handler handler = new Handler();
         this.setEstado(this.CASA);
-        int puntuacionaSuperar = this.puntJugador;
         do{
             Carta carta=this.baraja.getCarta();
             this.setPuntMaquina(this.puntMaquina + carta.getValue());
-            this.imagenCarta.setImageResource(this.getResources().getIdentifier("DM_BlackJackApp:"+carta.getImage(),null,null));
-            //funcion retardo
-            try {
-                Thread.sleep(1500);
-            } catch (InterruptedException ex) {
-// aquí tratamos la excepción como queramos, haciendo nada, sacando por pantalla el error, ...
-            }
-        }while (this.puntMaquina >= 21 || this.puntMaquina > puntuacionaSuperar);
+            this.imagenCarta.setImageResource(this.getResources().getIdentifier(carta.getImage(),"drawable",getPackageName()));
+            handler.postDelayed(new Runnable(){public void run(){}},3000);
+        }while (this.puntMaquina<21 && this.puntMaquina<this.puntJugador);
 
+        if(this.puntJugador<=21 && this.puntMaquina>21){ //jugador gana la partida
+            this.setDinGanado(this.dinGanado+this.dinApostado*2);
+        }
+        else if(this.puntMaquina==this.puntJugador){ //jugador y maquina empatan
+            this.setDinGanado(this.dinGanado+this.dinApostado);
+        }
+
+        this.setDinApostado(0);
+        handler.postDelayed(new Runnable(){public void run(){}},3000);
+
+        this.setPuntJugador(0);
+        this.setPuntMaquina(0);
+        this.imagenCarta.setImageResource(R.drawable.tapas);
+        this.setEstado(this.CARTA1);
     }
 
     private void setPuntJugador(int punt){ //cambia puntuacion jugador y lo muestra en la vista
         this.puntJugador=punt;
-        TextView textPuntJugador=this.findViewById(R.id.puntPropia);
-        textPuntJugador.setText("Puntuacion jugador: "+this.puntJugador);
+        this.textPuntJugador.setText("Puntuacion jugador: "+this.puntJugador);
     }
 
     private void setPuntMaquina(int punt){ //cambia puntuacion maquina y lo muestra en la vista
         this.puntMaquina=punt;
-        TextView textPuntCasa=this.findViewById(R.id.puntCasa);
-        textPuntCasa.setText("Puntuacion casa: "+this.puntMaquina);
+        this.textPuntCasa.setText("Puntuacion casa: "+this.puntMaquina);
     }
 
     private void setDinGanado(int din){ //cambia dinero total y lo muestra en la vista
         this.dinGanado=din;
-        TextView dinero=this.findViewById(R.id.dinero);
-        dinero.setText("Dinero total: "+this.dinGanado);
+        this.dinero.setText("Dinero total: "+this.dinGanado);
     }
 
     private void setDinApostado(int din){ //cambia apuesta y lo muestra en la vista
         this.dinApostado=din;
-        TextView apuesta=this.findViewById(R.id.apostadoTotal);
-        apuesta.setText("Apostado: "+this.dinGanado);
+        this.setDinGanado(this.dinGanado-din); //din apostado = din perdido
+        this.apuesta.setText("Apostado: "+this.dinApostado);
     }
 
-    private void setEstado(byte est){ //cambia turnos y habilita/deshabilita botones
+    private void setEstado(int est){ //cambia turnos y habilita/deshabilita botones
         this.estado=est;
-        TextView turno=this.findViewById(R.id.turno);
         switch (est){
             case CARTA1:
-                turno.setText("Turno: Jugador");
+                this.turno.setText("Turno: Jugador");
                 this.btnApostar.setEnabled(true);
                 this.btnRecibir.setEnabled(false);
                 this.btnPlantarse.setEnabled(false);
+                break;
             case CARTA2:
-                turno.setText("Turno: Jugador");
+                this.turno.setText("Turno: Jugador");
                 this.btnApostar.setEnabled(true);
                 this.btnRecibir.setEnabled(true);
                 this.btnPlantarse.setEnabled(true);
+                break;
             case MAS21:
-                turno.setText("Turno: Jugador");
+                this.turno.setText("Turno: Jugador");
                 this.btnApostar.setEnabled(false);
                 this.btnRecibir.setEnabled(false);
                 this.btnPlantarse.setEnabled(true);
+                break;
             case CASA:
-                turno.setText("Turno: Casa");
+                this.turno.setText("Turno: Casa");
                 this.btnApostar.setEnabled(false);
                 this.btnRecibir.setEnabled(false);
                 this.btnPlantarse.setEnabled(false);
+                break;
         }
     }
 }
